5c06cf644306ed671915f197c19f6fe198654a5f
Kyle L. Jensen <kljensen@gmail.com>
2020-12-01 16:43:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Golang Web Hello World
+
+This is a small "hello world" that I use for some testing.
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+    "os"
+)
+
+func getEnv(key string, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":"+getEnv("PORT", "8080"), nil)
+}
+
